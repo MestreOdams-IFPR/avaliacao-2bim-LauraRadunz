40d7e30700311bd272943ebac8a211719c786df6
LauraRadunz <lauraradunzpedro@gmail.com>
2025-07-02 11:07:48 -0300

    02/07/2025

--- Prova.java
+++ Prova.java
@@ -1,14 +1,66 @@
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Prova {
     static Scanner teclado = new Scanner(System.in);
 
     public static void main(String[] args) {
-        //exercicio1();
-        // exercicio2();
-        testeForca();
+        // respostaRecursivo();
+        // recursvivo1();
+        // recursivo2();
+        // testeForca();
+        // int casosFraseCompleta = teclado.nextInt();
+        // fraseCompleta(casosFraseCompleta);
+        buscaCristais();
+    }
+
+    public static void buscaCristais() {
+        int caso = 0;
+        while (true) {
+            int cristaisKyber = teclado.nextInt();
+            int consultas = teclado.nextInt();
+            if (cristaisKyber == 0 && consultas == 0) {
+                break;
+            }
+            int[] numerosKyber = criarVetor(cristaisKyber);
+            numerosKyber = lerVetor(numerosKyber);
+
+            numerosKyber = mergeSort(numerosKyber);
+            numerosKyber = inverterVetor(numerosKyber, 0, cristaisKyber-1);
+
+            for (int i = 0; i < consultas; i++) {
+                int numero = teclado.nextInt();
+                int valor = numero;
+                numero = buscaBinaria(numerosKyber, numero, 0, cristaisKyber-1);
+                caso++;
+                System.out.println("CASE# "+caso+":");
+                if (numero==-1) {
+                    System.out.println(valor +" not found");
+                    System.out.println();
+                } else {
+                    System.out.println(valor+" found at "+numero);
+                    System.out.println();
+                }
+            }
+
+        }
+
+    }
+
+    public static int buscaBinaria(int[] v, int num, int inicio, int fim) {
+        int meio = (inicio+fim)/2;
+        if (inicio==fim&&inicio!=num) {
+            return -1;
+        }
+        if (v[meio] == num) {
+            while (meio>0 && v[meio-1] == num) {
+                meio --;
+            }
+            return meio;
+        } else if (num<v[meio]) {
+            return buscaBinaria(v, num, inicio, meio-1);
+        } else {
+            return buscaBinaria(v, num, meio+1, fim);
+        }
     }
 
     public static void testeForca() {
@@ -18,7 +70,7 @@ public class Prova {
             int k = teclado.nextInt();
             pontuacoes = lerVetor(pontuacoes);
             pontuacoes = mergeSort(pontuacoes);
-            pontuacoes = inverterVetor(pontuacoes, 0, n-1);
+            pontuacoes = inverterVetor(pontuacoes, 0, n - 1);
             int soma = 0;
             for (int i = 0; i < k; i++) {
                 soma += pontuacoes[i];
@@ -33,93 +85,90 @@ public class Prova {
         }
         int temp = v[i];
         v[i] = v[j];
-        v[j] = v[i];
-        return inverterVetor(v, i+1, j-1);
+        v[j] = temp;
+        return inverterVetor(v, i + 1, j - 1);
     }
 
-    public static void fraseCompleta() {
-        String frase = teclado.nextLine();
-        String[] caracteres = frase.toLowerCase().split("");
-        String[] alfabeto = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
-
-        for (String n : caracteres) {
-        for (int i = 0; i < alfabeto.length; i++) {
-            if (n == alfabeto[i]) {
-                alfabeto[i] = "0";
+    public static void fraseCompleta(int casos) {
+        teclado.next();
+        int i = 0;
+        while (true) {
+            if (i == casos) {
+                break;
             }
-        }
-        }
+            String frase = teclado.nextLine();
+            String[] caracteres = frase.toLowerCase().split("");
+            String[] alfabeto = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
+                    "r", "s", "t", "u", "v", "w", "x", "y", "z" };
 
-        int zeros = 0;
-        for (int i = 0; i < alfabeto.length; i++) {
-            if (!alfabeto[i].equals("0")) {
-                zeros++;
+            for (String n : caracteres) {
+                for (int j = 0; j < alfabeto.length; j++) {
+                    if (n.equals(alfabeto[j])) {
+                        alfabeto[j] = "0";
+                    }
+                }
             }
+
+            int zeros = 0;
+            for (int j = 0; j < alfabeto.length; j++) {
+                if (alfabeto[j].equals("0")) {
+                    zeros++;
+                }
+            }
+            String resposta = formularResposta(zeros);
+            System.out.println(resposta);
+            i++;
         }
 
-        if (zeros==26) {
-            String resposta = “frasecompleta”;
-        }
     }
 
-    public static void exercicio2() {
+    public static String formularResposta(int zeros) {
+        if (zeros == 26) {
+            return "frasecompleta";
+        }
+        if (zeros >= 13) {
+            return "frase quase completa";
+        }
+        return "frase mal elaborada";
+    }
+
+    public static void recursivo2() {
         int n = teclado.nextInt();
-        n = tribonacci(n, 0, 0, 0, 1, 3);
+        n = pell(n);
         System.out.println(n);
     }
 
-    // public static int pell(int n) {
-    //     if (n == 0) {
-    //         return valor;
-    //     }
-    //     valor =  2*pell(n-1) + pell(n=2);
+    public static void recursvivo1() {
+        int n = teclado.nextInt();
+        n = tribonacci(n);
+        System.out.println(n);
+    }
 
-    // }
-
-    public static int tribonacci(int n, int valor, int a, int b, int c, int d) {
-        if (n == 3) {
-            return 1;
-        } else if (n == 1 || n ==2) {
+    public static int pell(int n) {
+        if (n == 0) {
             return 0;
-        } if (d == n) {
-            return valor;
-        }    
-        a = b;
-        b = c;
-        return tribonacci(n, c, a, b, a+b, d+1);
+        }
+        if (n == 1) {
+            return 1;
+        }
+        return 2 * pell(n - 1) + pell(n - 2);
 
     }
 
-    public static void exercicio1() {
-        //O método está invertendo um vetor
+    public static int tribonacci(int n) {
+        if (n == 0 || n == 1) {
+            return 0;
+        } else if (n == 2) {
+            return 1;
+        }
+        return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
     }
 
-    //0 0 1 1 2 4 7 
+    public static void respostaRecursivo() {
+        // O método está invertendo um vetor
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    // 0 0 1 1 2 4 7
 
     public static int quickSort(int[] v) {
         int movimentos = quickSortRec(v, 0, v.length - 1);
@@ -164,7 +213,6 @@ public class Prova {
         v[j] = temp;
         return v;
     }
-
 
     public static int[] mergeSort(int[] v) {
         if (v.length < 2) {
